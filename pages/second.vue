95908e944a28779741ebcec79ed1e4fc3c5c79a8
bojar <michael.sagel89@gmail.com>
2021-12-16 17:11:00 +0100

    timer

---
+++ pages/second.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="base-timer">
+    <svg
+      class="base-timer__svg"
+      viewBox="0 0 100 100"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g class="base-timer__circle">
+        <circle
+          class="base-timer__path-elapsed"
+          cx="50"
+          cy="50"
+          r="46.5"
+        />
+      </g>
+    </svg>
+    <span class="base-timer__label">
+      <!-- Remaining time label -->
+    </span>
+
+
+
+
+
+
+
+<svg viewBox="0 0 300 100" xmlns="http://www.w3.org/2000/svg" stroke="red" fill="grey">
+  <circle cx="50" cy="50" r="40" />
+  <circle cx="150" cy="50" r="4" />
+
+  <svg viewBox="0 0 10 10" x="200" width="100">
+    <circle cx="5" cy="5" r="4" />
+  </svg>
+</svg>
+
+
+
+
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+/* Sets the containers height and width */
+.base-timer {
+  position: relative;
+  width: 300px;
+  height: 300px;
+/* Removes SVG styling that would hide the time label */
+  &__circle {
+    fill: none;
+    stroke: none;
+  }
+/* The SVG path that displays the timer's progress */
+  &__path-elapsed {
+    stroke-width: 7px;
+    stroke:grey;
+  }
+}
+</style>
+
+
+
+
+
