c135eccce9e760e7c4dab10a18c95072b8d68c54
bojar <michael.sagel89@gmail.com>
2021-12-20 23:37:30 +0100

    menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="test-index">
-    <button
-      class="btn-primary"
-      @click="refresh"
-    >
-      <template v-if="isHidden">Click to Show</template>
-      <template v-else>Click to Hide</template>
-    </button>
-
-    <button 
-      class="test"
-      @click="isStarted = !isStarted"
-    >
-     {{ isStarted ? 'Stop' : 'Start' }}
-    </button>
+    
   </div>
 </template>
 
@@ -22,16 +9,10 @@ export default {
 
   data: function () {
     return {
-      isHidden: false,
-      isStarted: false,
     }
   },
   colorMode: "light",
   methods: {
-    refresh: function() {
-      this.isHidden = !this.isHidden
-      this.$nuxt.refresh();
-    },
   },
 }
 </script>
--- pages/second.vue
+++ pages/second.vue
@@ -8,7 +8,7 @@
                 <div
                     class="menu-item pattens-blue"
                 >
-                    <img class="menu-img" src="./../assets/images/second/plate__french-fries.png" />
+                    <img class="menu-img" src="./../assets/images/second/plate__french-fries.png"/>
                     <div class="menu-item-info">
                         <div
                             class="menu-title"
@@ -18,14 +18,14 @@
                         <div 
                             class="price"
                         >
-                            $2.23
+                            ${{priceFrenchFries}}
                         </div>
                         <div
                             class="btn-bay"
-                            id="myDiv"
-                            @click="bought"
+                            id="btnFrenchFries"
+                            @click="addFrenchFries"
                         >
-                            <template v-if="isBought">
+                            <template v-if="isAddFrenchFries">
                                 Add to Cart
                             </template>
                             <template v-else >
@@ -38,7 +38,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="menu-item pale-rose">
+                <div 
+                    class="menu-item pale-rose">
                     <img class="menu-img" src="./../assets/images/second/plate__salmon-vegetables.png" />
                     <div class="menu-item-info">
                         <div
@@ -49,14 +50,14 @@
                         <div 
                             class="price"
                         >
-                            $5.12
+                            ${{priceSalmonAndVegetables}}
                         </div>
                         <div
                             class="btn-bay"
-                            id="myDiv"
-                            @click="bought"
+                            id="btnSalmonAndVegetables"
+                            @click="addSalmonAndVegetables"
                         >
-                            <template v-if="isBought">
+                            <template v-if="isAddSalmonAndVegetables">
                                 Add to Cart
                             </template>
                             <template v-else >
@@ -82,14 +83,14 @@
                         <div 
                             class="price"
                         >
-                            $7.82
+                            ${{priceSpaghettiWithMeatSauce}}
                         </div>
                         <div
                             class="btn-bay"
-                            id="myDiv"
-                            @click="bought"
+                            id="btnSpaghettiWithMeatSauce"
+                            @click="addSpaghettiWithMeatSauce"
                         >
-                            <template v-if="isBought">
+                            <template v-if="isAddSpaghettiWithMeatSauce">
                                 Add to Cart
                             </template>
                             <template v-else >
@@ -115,14 +116,14 @@
                         <div 
                             class="price"
                         >
-                            $6.98
+                            ${{priceChickenSaladwithParmesean}}
                         </div>
                         <div
                             class="btn-bay"
-                            id="myDiv"
-                            @click="bought"
+                            id="btnChickenSaladwithParmesean"
+                            @click="addChickenSaladwithParmesean"
                         >
-                            <template v-if="isBought">
+                            <template v-if="isAddChickenSaladwithParmesean">
                                 Add to Cart
                             </template>
                             <template v-else >
@@ -148,14 +149,14 @@
                         <div 
                             class="price"
                         >
-                            $6.34
+                            ${{priceFishSticksAndFries}}
                         </div>
                         <div
                             class="btn-bay"
-                            id="myDiv"
-                            @click="bought"
+                            id="btnFishSticksAndFries"
+                            @click="addFishSticksAndFries"
                         >
-                            <template v-if="isBought">
+                            <template v-if="isAddFishSticksAndFries">
                                 Add to Cart
                             </template>
                             <template v-else >
@@ -181,14 +182,14 @@
                         <div 
                             class="price"
                         >
-                            $6.45
+                            ${{priceRavioli}}
                         </div>
                         <div
                             class="btn-bay"
-                            id="myDiv"
-                            @click="bought"
+                            id="btnRavioli"
+                            @click="addRavioli"
                         >
-                            <template v-if="isBought">
+                            <template v-if="isAddRavioli">
                                 Add to Cart
                             </template>
                             <template v-else >
@@ -214,14 +215,14 @@
                         <div 
                             class="price"
                         >
-                            $6.05
+                            ${{priceTortellini}}
                         </div>
                         <div
                             class="btn-bay"
-                            id="myDiv"
-                            @click="bought"
+                            id="btnTortellini"
+                            @click="addTortellini"
                         >
-                            <template v-if="isBought">
+                            <template v-if="isAddTortellini">
                                 Add to Cart
                             </template>
                             <template v-else >
@@ -247,14 +248,14 @@
                         <div 
                             class="price"
                         >
-                            $5.99
+                            ${{priceBaconEggsAndToast}}
                         </div>
                         <div
                             class="btn-bay"
-                            id="myDiv"
-                            @click="bought"
+                            id="btnBaconEggsAndToast"
+                            @click="addBaconEggsAndToast"
                         >
-                            <template v-if="isBought">
+                            <template v-if="isAddBaconEggsAndToast">
                                 Add to Cart
                             </template>
                             <template v-else >
@@ -270,8 +271,59 @@
             </div>
         </div>
 
-        <div class="card"
-        >
+        <div class="card">
+            <div class="card-content">
+                <div class="your-card">
+                    Your Card
+                </div>
+
+
+                <div
+                    class="card-item"
+                >
+                    <template v-if="!isAddFrenchFries">
+                        <div class="numberOfOrders">
+                            {{numberOfFrenchFries}}
+                        <img class="card-img" src="./../assets/images/second/plate__french-fries.png"/>
+                        </div>
+                        <div>
+                            Fish Sticks and Fries
+                            <div>{{priceFrenchFries}}</div>
+                        </div>
+                        <div>
+                            <img class="minus-img" src="./../assets/images/second/chevron.svg"/>
+                            {{numberOfFrenchFries}}
+                            <img class="plus-img" src="./../assets/images/second/chevron.svg"/>
+                        </div>
+                    </template>
+                </div>
+
+
+
+
+                <div class="card-line"></div>
+            </div>
+                
+            <div class="sum">
+                <div class="sum-layout">
+                    Subtotal:
+                    <div class="sum-container">
+                        $ {{subtotal}}
+                    </div>
+                </div>
+                <div class="sum-layout">
+                    Tax:
+                    <div class="sum-container">
+                        $ {{Math.floor((subtotal * 0.0975)* 100) / 100}}
+                    </div>
+                </div>
+                <div class="sum-layout">
+                    Total:
+                    <div class="sum-container">
+                        $ {{Math.floor((subtotal + (subtotal * 0.0975))* 100) / 100 }}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -283,17 +335,104 @@ export default {
     colorMode: "second",
     data: function () {
     return {
-      isBought: true,
+        numberOfFrenchFries: 1,
+        subtotal: 10.8,
+        priceFrenchFries: 2.23,
+        priceSalmonAndVegetables: 5.12,
+        priceSpaghettiWithMeatSauce: 7.82,
+        priceChickenSaladwithParmesean: 6.98,
+        priceFishSticksAndFries:6.34,
+        priceRavioli: 6.45,
+        priceTortellini: 6.05,
+        priceBaconEggsAndToast: 5.99,
+        isAddFrenchFries: true,
+        isAddSalmonAndVegetables: true,
+        isAddSpaghettiWithMeatSauce: true,
+        isAddChickenSaladwithParmesean: true,
+        isAddFishSticksAndFries: true,
+        isAddRavioli:true,
+        isAddTortellini:true,
+        isAddBaconEggsAndToast: true,
     }
   },
   methods:{
-    bought: function(){
-        this.isBought = !this.isBought
-        if (this.isBought === true){
-            var element = document.getElementById("myDiv");
+    addFrenchFries: function(){
+        this.isAddFrenchFries = !this.isAddFrenchFries
+        if (this.isAddFrenchFries === true){
+            var element = document.getElementById("btnFrenchFries");
             element.style.backgroundColor = "#6B00F5";
-        }else if (this.isBought === false){
-            var element = document.getElementById("myDiv");
+        }else if (this.isAddFrenchFries === false){
+            var element = document.getElementById("btnFrenchFries");
+            element.style.backgroundColor = "black";
+        }
+    },
+    addSalmonAndVegetables: function(){
+        this.isAddSalmonAndVegetables = !this.isAddSalmonAndVegetables
+        if (this.isAddSalmonAndVegetables === true){
+            var element = document.getElementById("btnSalmonAndVegetables");
+            element.style.backgroundColor = "#6B00F5";
+        }else if (this.isAddSalmonAndVegetables === false){
+            var element = document.getElementById("btnSalmonAndVegetables");
+            element.style.backgroundColor = "black";
+        }
+    },
+    addSpaghettiWithMeatSauce: function(){
+        this.isAddSpaghettiWithMeatSauce = !this.isAddSpaghettiWithMeatSauce
+        if (this.isAddSpaghettiWithMeatSauce === true){
+            var element = document.getElementById("btnSpaghettiWithMeatSauce");
+            element.style.backgroundColor = "#6B00F5";
+        }else if (this.isAddSpaghettiWithMeatSauce === false){
+            var element = document.getElementById("btnSpaghettiWithMeatSauce");
+            element.style.backgroundColor = "black";
+        }
+    },
+    addChickenSaladwithParmesean: function(){
+        this.isAddChickenSaladwithParmesean = !this.isAddChickenSaladwithParmesean
+        if (this.isAddChickenSaladwithParmesean === true){
+            var element = document.getElementById("btnChickenSaladwithParmesean");
+            element.style.backgroundColor = "#6B00F5";
+        }else if (this.isAddChickenSaladwithParmesean === false){
+            var element = document.getElementById("btnChickenSaladwithParmesean");
+            element.style.backgroundColor = "black";
+        }
+    },
+    addFishSticksAndFries: function(){
+        this.isAddFishSticksAndFries = !this.isAddFishSticksAndFries
+        if (this.isAddFishSticksAndFries === true){
+            var element = document.getElementById("btnFishSticksAndFries");
+            element.style.backgroundColor = "#6B00F5";
+        }else if (this.isAddFishSticksAndFries === false){
+            var element = document.getElementById("btnFishSticksAndFries");
+            element.style.backgroundColor = "black";
+        }
+    },
+    addRavioli: function(){
+        this.isAddRavioli = !this.isAddRavioli
+        if (this.isAddRavioli === true){
+            var element = document.getElementById("btnRavioli");
+            element.style.backgroundColor = "#6B00F5";
+        }else if (this.isAddRavioli === false){
+            var element = document.getElementById("btnRavioli");
+            element.style.backgroundColor = "black";
+        }
+    },
+    addTortellini: function(){
+        this.isAddTortellini = !this.isAddTortellini
+        if (this.isAddTortellini === true){
+            var element = document.getElementById("btnTortellini");
+            element.style.backgroundColor = "#6B00F5";
+        }else if (this.isAddTortellini === false){
+            var element = document.getElementById("btnTortellini");
+            element.style.backgroundColor = "black";
+        }
+    },
+    addBaconEggsAndToast: function(){
+        this.isAddBaconEggsAndToast = !this.isAddBaconEggsAndToast
+        if (this.isAddBaconEggsAndToast === true){
+            var element = document.getElementById("btnBaconEggsAndToast");
+            element.style.backgroundColor = "#6B00F5";
+        }else if (this.isAddBaconEggsAndToast === false){
+            var element = document.getElementById("btnBaconEggsAndToast");
             element.style.backgroundColor = "black";
         }
     }
@@ -305,8 +444,3 @@ export default {
 <style scoped lang="scss">
 @import "./../assets/styles/second.scss";
 </style>
-
-
-
-
-
