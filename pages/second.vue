dda932c7b7acf43a31af3d8b0f73bb16aa348a9e
bojar <michael.sagel89@gmail.com>
2021-12-22 02:32:33 +0100

    menu

--- pages/second.vue
+++ pages/second.vue
@@ -272,34 +272,150 @@
         </div>
 
         <div class="card">
+            <div class="your-card">
+                Your Card
+            </div>
             <div class="card-content">
-                <div class="your-card">
-                    Your Card
-                </div>
-
-
                 <div
                     class="card-item"
                 >
                     <template v-if="!isAddFrenchFries">
-                        <div class="numberOfOrders">
-                            {{numberOfFrenchFries}}
-                        <img class="card-img" src="./../assets/images/second/plate__french-fries.png"/>
+                        <div class="card-item-tamplate">
+                            <img class="card-img" src="./../assets/images/second/plate__french-fries.png"/>
+                            <div class="numberOfOrders">
+                                {{numberOfFrenchFries}}
+                            </div>
+                            <div class="card-info">
+                                <div>
+                                    Fish Sticks and Fries
+                                </div>
+                                <div class="card-info-price">${{priceFrenchFries}}</div>
+                            
+                                <div class="amount-of-orders">
+                                    <div 
+                                        @click="removeFrenchFriesNumber"
+                                        class="minus-img"
+                                    > 
+                                        <img src="./../assets/images/second/chevron.svg"/>
+                                    </div>
+                                        {{numberOfFrenchFries}}
+                                    <div 
+                                        @click="addFrenchFriesNumber"
+                                        class="plus-img"
+                                    >
+                                        <img src="./../assets/images/second/chevron.svg"/>
+                                    </div>
+                                    <div class="amount-of-orders-price">
+                                        ${{totalPriceFrenchFries}}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            Fish Sticks and Fries
-                            <div>{{priceFrenchFries}}</div>
+                        <div class="card-line-tamplate"></div>
+                    </template>
+                    <template v-if="!isAddFrenchFries">
+                        <div class="card-item-tamplate">
+                            <img class="card-img" src="./../assets/images/second/plate__french-fries.png"/>
+                            <div class="numberOfOrders">
+                                {{numberOfFrenchFries}}
+                            </div>
+                            <div class="card-info">
+                                <div>
+                                    Fish Sticks and Fries
+                                </div>
+                                <div class="card-info-price">${{priceFrenchFries}}</div>
+                            
+                                <div class="amount-of-orders">
+                                    <div 
+                                        @click="removeFrenchFriesNumber"
+                                        class="minus-img"
+                                    > 
+                                        <img src="./../assets/images/second/chevron.svg"/>
+                                    </div>
+                                        {{numberOfFrenchFries}}
+                                    <div 
+                                        @click="addFrenchFriesNumber"
+                                        class="plus-img"
+                                    >
+                                        <img src="./../assets/images/second/chevron.svg"/>
+                                    </div>
+                                    <div class="amount-of-orders-price">
+                                        ${{totalPriceFrenchFries}}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <img class="minus-img" src="./../assets/images/second/chevron.svg"/>
-                            {{numberOfFrenchFries}}
-                            <img class="plus-img" src="./../assets/images/second/chevron.svg"/>
+                        <div class="card-line-tamplate"></div>
+                    </template>
+                    <template v-if="!isAddFrenchFries">
+                        <div class="card-item-tamplate">
+                            <img class="card-img" src="./../assets/images/second/plate__french-fries.png"/>
+                            <div class="numberOfOrders">
+                                {{numberOfFrenchFries}}
+                            </div>
+                            <div class="card-info">
+                                <div>
+                                    Fish Sticks and Fries
+                                </div>
+                                <div class="card-info-price">${{priceFrenchFries}}</div>
+                            
+                                <div class="amount-of-orders">
+                                    <div 
+                                        @click="removeFrenchFriesNumber"
+                                        class="minus-img"
+                                    > 
+                                        <img src="./../assets/images/second/chevron.svg"/>
+                                    </div>
+                                        {{numberOfFrenchFries}}
+                                    <div 
+                                        @click="addFrenchFriesNumber"
+                                        class="plus-img"
+                                    >
+                                        <img src="./../assets/images/second/chevron.svg"/>
+                                    </div>
+                                    <div class="amount-of-orders-price">
+                                        ${{totalPriceFrenchFries}}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        <div class="card-line-tamplate"></div>
+                    </template>
+                    <template v-if="!isAddFrenchFries">
+                        <div class="card-item-tamplate">
+                            <img class="card-img" src="./../assets/images/second/plate__french-fries.png"/>
+                            <div class="numberOfOrders">
+                                {{numberOfFrenchFries}}
+                            </div>
+                            <div class="card-info">
+                                <div>
+                                    Fish Sticks and Fries
+                                </div>
+                                <div class="card-info-price">${{priceFrenchFries}}</div>
+                            
+                                <div class="amount-of-orders">
+                                    <div 
+                                        @click="removeFrenchFriesNumber"
+                                        class="minus-img"
+                                    > 
+                                        <img src="./../assets/images/second/chevron.svg"/>
+                                    </div>
+                                        {{numberOfFrenchFries}}
+                                    <div 
+                                        @click="addFrenchFriesNumber"
+                                        class="plus-img"
+                                    >
+                                        <img src="./../assets/images/second/chevron.svg"/>
+                                    </div>
+                                    <div class="amount-of-orders-price">
+                                        ${{totalPriceFrenchFries}}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-line-tamplate"></div>
                     </template>
                 </div>
-
-
-
 
                 <div class="card-line"></div>
             </div>
@@ -335,8 +451,9 @@ export default {
     colorMode: "second",
     data: function () {
     return {
+        totalPriceFrenchFries: 2.23,
         numberOfFrenchFries: 1,
-        subtotal: 10.8,
+        subtotal: 0,
         priceFrenchFries: 2.23,
         priceSalmonAndVegetables: 5.12,
         priceSpaghettiWithMeatSauce: 7.82,
@@ -353,27 +470,33 @@ export default {
         isAddRavioli:true,
         isAddTortellini:true,
         isAddBaconEggsAndToast: true,
+        orderPrice: 0
     }
   },
   methods:{
     addFrenchFries: function(){
         this.isAddFrenchFries = !this.isAddFrenchFries
-        if (this.isAddFrenchFries === true){
-            var element = document.getElementById("btnFrenchFries");
-            element.style.backgroundColor = "#6B00F5";
-        }else if (this.isAddFrenchFries === false){
+        if (this.isAddFrenchFries === false){
             var element = document.getElementById("btnFrenchFries");
             element.style.backgroundColor = "black";
+            this.orderPrice = this.orderPrice + this.priceFrenchFries;
+        }else if (this.isAddFrenchFries === true){
+            var element = document.getElementById("btnFrenchFries");
+            element.style.backgroundColor = "#6B00F5";
+            this.orderPrice = 0;
         }
+        this.subtotal = this.orderPrice
     },
     addSalmonAndVegetables: function(){
         this.isAddSalmonAndVegetables = !this.isAddSalmonAndVegetables
         if (this.isAddSalmonAndVegetables === true){
             var element = document.getElementById("btnSalmonAndVegetables");
             element.style.backgroundColor = "#6B00F5";
+            this.subtotal = this.subtotal - this.priceSalmonAndVegetables;
         }else if (this.isAddSalmonAndVegetables === false){
             var element = document.getElementById("btnSalmonAndVegetables");
             element.style.backgroundColor = "black";
+            this.subtotal = this.subtotal + this.priceSalmonAndVegetables;
         }
     },
     addSpaghettiWithMeatSauce: function(){
@@ -381,9 +504,11 @@ export default {
         if (this.isAddSpaghettiWithMeatSauce === true){
             var element = document.getElementById("btnSpaghettiWithMeatSauce");
             element.style.backgroundColor = "#6B00F5";
+            this.subtotal = this.subtotal - this.priceSpaghettiWithMeatSauce;
         }else if (this.isAddSpaghettiWithMeatSauce === false){
             var element = document.getElementById("btnSpaghettiWithMeatSauce");
             element.style.backgroundColor = "black";
+            this.subtotal = this.subtotal + this.priceSpaghettiWithMeatSauce;
         }
     },
     addChickenSaladwithParmesean: function(){
@@ -391,9 +516,11 @@ export default {
         if (this.isAddChickenSaladwithParmesean === true){
             var element = document.getElementById("btnChickenSaladwithParmesean");
             element.style.backgroundColor = "#6B00F5";
+            this.subtotal = this.subtotal - this.priceChickenSaladwithParmesean;
         }else if (this.isAddChickenSaladwithParmesean === false){
             var element = document.getElementById("btnChickenSaladwithParmesean");
             element.style.backgroundColor = "black";
+            this.subtotal = this.subtotal + this.priceChickenSaladwithParmesean;
         }
     },
     addFishSticksAndFries: function(){
@@ -401,9 +528,11 @@ export default {
         if (this.isAddFishSticksAndFries === true){
             var element = document.getElementById("btnFishSticksAndFries");
             element.style.backgroundColor = "#6B00F5";
+            this.subtotal = this.subtotal - this.priceFishSticksAndFries;
         }else if (this.isAddFishSticksAndFries === false){
             var element = document.getElementById("btnFishSticksAndFries");
             element.style.backgroundColor = "black";
+            this.subtotal = this.subtotal + this.priceFishSticksAndFries;
         }
     },
     addRavioli: function(){
@@ -411,9 +540,11 @@ export default {
         if (this.isAddRavioli === true){
             var element = document.getElementById("btnRavioli");
             element.style.backgroundColor = "#6B00F5";
+            this.subtotal = this.subtotal - this.priceRavioli;
         }else if (this.isAddRavioli === false){
             var element = document.getElementById("btnRavioli");
             element.style.backgroundColor = "black";
+            this.subtotal = this.subtotal + this.priceRavioli;
         }
     },
     addTortellini: function(){
@@ -421,9 +552,11 @@ export default {
         if (this.isAddTortellini === true){
             var element = document.getElementById("btnTortellini");
             element.style.backgroundColor = "#6B00F5";
+            this.subtotal = this.subtotal - this.priceTortellini;
         }else if (this.isAddTortellini === false){
             var element = document.getElementById("btnTortellini");
             element.style.backgroundColor = "black";
+            this.subtotal = this.subtotal + this.priceTortellini;
         }
     },
     addBaconEggsAndToast: function(){
@@ -431,11 +564,31 @@ export default {
         if (this.isAddBaconEggsAndToast === true){
             var element = document.getElementById("btnBaconEggsAndToast");
             element.style.backgroundColor = "#6B00F5";
+            this.subtotal = this.subtotal - this.priceBaconEggsAndToast;
         }else if (this.isAddBaconEggsAndToast === false){
             var element = document.getElementById("btnBaconEggsAndToast");
             element.style.backgroundColor = "black";
+            this.subtotal = this.subtotal + this.priceBaconEggsAndToast;
         }
-    }
+    },
+    addFrenchFriesNumber: function() {
+        this.numberOfFrenchFries = this.numberOfFrenchFries + 1
+        this.totalPriceFrenchFries = this.totalPriceFrenchFries + this.priceFrenchFries
+            this.subtotal = this.subtotal + this.priceFrenchFries;
+    },
+    removeFrenchFriesNumber: function() {
+        if (this.numberOfFrenchFries > 1) {
+            this.numberOfFrenchFries = this.numberOfFrenchFries - 1;
+        this.totalPriceFrenchFries = this.totalPriceFrenchFries - this.priceFrenchFries
+            this.subtotal = this.subtotal - this.priceFrenchFries;
+        }else if (this.numberOfFrenchFries === 1){
+            this.isAddFrenchFries = true
+            var element = document.getElementById("btnFrenchFries");
+            element.style.backgroundColor = "#6B00F5";
+            this.subtotal = this.subtotal - this.priceFrenchFries;
+            
+        }
+    },
   },
 }
 </script>
