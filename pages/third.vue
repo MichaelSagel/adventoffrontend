eed20f9b92fbb0edc57acffcf5467b0ed5a61b93
bojar <michael.sagel89@gmail.com>
2022-02-01 16:03:43 +0100

    piano

--- pages/third.vue
+++ pages/third.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="third">
-   test
+    <img class="background-img" src="./../assets/images/third/piano.svg" />
+    <div class="white">
+      <div class="firstWhiteKey yellow"></div>
+      <div class="secondWhiteKey yellow"></div>
+      <div class="thirdWhiteKey yellow"></div>
+      <div class="fourthWhiteKey yellow"></div>
+      <div class="fifthWhiteKey yellow"></div>
+      <div class="sixthWhiteKey yellow"></div>
+      <div class="seventhWhiteKey yellow"></div>
+      <div class="eighthWhiteKey yellow"></div>
+      <div class="ninthWhiteKey yellow"></div>
+      <div class="tenthWhiteKey yellow"></div>
+      <div class="eleventhWhiteKey yellow"></div>
+      <div class="twelfthWhiteKey yellow"></div>
+      <div class="thirteenthWhiteKey yellow"></div>
+    </div>
+    <div class="black">
+      <div class="firstBlackKey pink"></div>
+       <div class="secondBlackKey pink"></div>
+     <div class="thirdBlackKey pink"></div>
+      <!-- <div class="fourthBlackKey pink"></div>
+      <div class="fifthBlackKey pink"></div>
+      <div class="sixthBlackKey pink"></div>
+      <div class="seventhBlackKey pink"></div>
+      <div class="eighthBlackKey pink"></div>
+      <div class="ninthBlackKey pink"></div>
+      <div class="tenthBlackKey pink"></div> -->
+    </div>
   </div>
 </template>
 
