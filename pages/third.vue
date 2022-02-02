9515ce27247f296b15fb6d9a54d3c110f798c2f6
bojar <michael.sagel89@gmail.com>
2022-02-02 23:18:35 +0100

    piano

--- pages/third.vue
+++ pages/third.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="third">
-    <div class="white">
       <button
         class="firstWhiteKey yellow"
         @click="playSound(sound1)"
@@ -53,8 +52,6 @@
         class="thirteenthWhiteKey yellow"
         @click="playSound(sound13)"
       ></button>
-    </div>
-    <div class="black">
       <button
         class="firstBlackKey pink"
         @click="playSound(sound14)"
@@ -95,34 +92,33 @@
         class="tenthBlackKey pink"
         @click="playSound(sound23)"
       ></button>
-    </div>
   </div>
 </template>
 
 <script>
-const sound1 = require("@/assets/audio/key-1.mp3").default;
-const sound2 = require("@/assets/audio/key-2.mp3").default;
-const sound3 = require("@/assets/audio/key-3.mp3").default;
-const sound4 = require("@/assets/audio/key-4.mp3").default;
-const sound5 = require("@/assets/audio/key-5.mp3").default;
-const sound6 = require("@/assets/audio/key-6.mp3").default;
-const sound7 = require("@/assets/audio/key-7.mp3").default;
-const sound8 = require("@/assets/audio/key-8.mp3").default;
-const sound9 = require("@/assets/audio/key-9.mp3").default;
-const sound10 = require("@/assets/audio/key-10.mp3").default;
-const sound11 = require("@/assets/audio/key-11.mp3").default;
-const sound12 = require("@/assets/audio/key-12.mp3").default;
-const sound13 = require("@/assets/audio/key-13.mp3").default;
-const sound14 = require("@/assets/audio/key-14.mp3").default;
-const sound15 = require("@/assets/audio/key-15.mp3").default;
-const sound16 = require("@/assets/audio/key-16.mp3").default;
-const sound17 = require("@/assets/audio/key-17.mp3").default;
-const sound18 = require("@/assets/audio/key-18.mp3").default;
-const sound19 = require("@/assets/audio/key-19.mp3").default;
-const sound20 = require("@/assets/audio/key-20.mp3").default;
-const sound21 = require("@/assets/audio/key-21.mp3").default;
-const sound22 = require("@/assets/audio/key-22.mp3").default;
-const sound23 = require("@/assets/audio/key-23.mp3").default;
+import sound1 from "@/assets/audio/key-1.mp3";
+import sound2  from "@/assets/audio/key-2.mp3";
+import sound3  from "@/assets/audio/key-3.mp3";
+import sound4  from "@/assets/audio/key-4.mp3";
+import sound5  from "@/assets/audio/key-5.mp3";
+import sound6  from "@/assets/audio/key-6.mp3";
+import sound7  from "@/assets/audio/key-7.mp3";
+import sound8  from "@/assets/audio/key-8.mp3";
+import sound9  from "@/assets/audio/key-9.mp3";
+import sound10  from "@/assets/audio/key-10.mp3";
+import sound11  from "@/assets/audio/key-11.mp3";
+import sound12  from "@/assets/audio/key-12.mp3";
+import sound13  from "@/assets/audio/key-13.mp3";
+import sound14  from "@/assets/audio/key-14.mp3";
+import sound15  from "@/assets/audio/key-15.mp3";
+import sound16  from "@/assets/audio/key-16.mp3";
+import sound17  from "@/assets/audio/key-17.mp3";
+import sound18  from "@/assets/audio/key-18.mp3";
+import sound19  from "@/assets/audio/key-19.mp3";
+import sound20  from "@/assets/audio/key-20.mp3";
+import sound21  from "@/assets/audio/key-21.mp3";
+import sound22  from "@/assets/audio/key-22.mp3";
+import sound23  from "@/assets/audio/key-23.mp3";
 export default {
   colorMode: "third",
   data() {
