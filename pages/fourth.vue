f69fe212900b2f90b2435d024b9486ca7cd1d111
bojar <michael.sagel89@gmail.com>
2022-02-16 16:34:37 +0100

    keyboard

--- pages/fourth.vue
+++ pages/fourth.vue
@@ -1,354 +1,457 @@
 <template>
-    <div class="fourth">
-        <h1>Eyes on the Screen</h1>
-        <div class="content">
-            <div class="Line">
-                <div class="regularButtons">{{keyboard[0].meaning}}</div>
-                <div class="regularButtons">{{keyboard[1].meaning}}</div>
-                <div class="regularButtons">{{keyboard[2].meaning}}</div>
-                <div class="regularButtons">{{keyboard[3].meaning}}</div>
-                <div class="regularButtons">{{keyboard[4].meaning}}</div>
-                <div class="regularButtons">{{keyboard[5].meaning}}</div>
-                <div class="regularButtons">{{keyboard[6].meaning}}</div>
-                <div class="regularButtons">{{keyboard[7].meaning}}</div>
-                <div class="regularButtons">{{keyboard[8].meaning}}</div>
-                <div class="regularButtons">{{keyboard[9].meaning}}</div>
-                <div class="regularButtons">{{keyboard[10].meaning}}</div>
-                <div class="regularButtons">{{keyboard[11].meaning}}</div>
-                <div class="regularButtons">{{keyboard[12].meaning}}</div>
-                <div class="regularButtons">{{keyboard[13].meaning}}</div>
-            </div>
-            <div class="Line">
-                <div class="regularButtons grayButtons">{{keyboard[14].meaning}}</div>
-                <div class="regularButtons">{{keyboard[15].meaning}}</div>
-                <div class="regularButtons">{{keyboard[16].meaning}}</div>
-                <div class="regularButtons">{{keyboard[17].meaning}}</div>
-                <div class="regularButtons">{{keyboard[18].meaning}}</div>
-                <div class="regularButtons">{{keyboard[19].meaning}}</div>
-                <div class="regularButtons">{{keyboard[20].meaning}}</div>
-                <div class="regularButtons">{{keyboard[21].meaning}}</div>
-                <div class="regularButtons">{{keyboard[22].meaning}}</div>
-                <div class="regularButtons">{{keyboard[23].meaning}}</div>
-                <div class="regularButtons">{{keyboard[24].meaning}}</div>
-                <div class="regularButtons">{{keyboard[25].meaning}}</div>
-                <div class="regularButtons">{{keyboard[26].meaning}}</div>
-            </div>
-            <div class="Line">
-                <div class="regularButtons grayButtons">{{keyboard[27].meaning}}</div>
-                <div class="regularButtons">{{keyboard[28].meaning}}</div>
-                <div class="regularButtons">{{keyboard[29].meaning}}</div>
-                <div class="regularButtons">{{keyboard[30].meaning}}</div>
-                <div class="regularButtons">{{keyboard[31].meaning}}</div>
-                <div class="regularButtons">{{keyboard[32].meaning}}</div>
-                <div class="regularButtons">{{keyboard[33].meaning}}</div>
-                <div class="regularButtons">{{keyboard[34].meaning}}</div>
-                <div class="regularButtons">{{keyboard[35].meaning}}</div>
-                <div class="regularButtons">{{keyboard[36].meaning}}</div>
-                <div class="regularButtons">{{keyboard[37].meaning}}</div>
-                <div class="regularButtons">{{keyboard[38].meaning}}</div>
-                <div class="regularButtons grayButtons">{{keyboard[39].meaning}}</div>
-            </div>
-            <div class="Line">
-                <div class="regularButtons grayButtons">{{keyboard[40].meaning}}</div>
-                <div class="regularButtons">{{keyboard[41].meaning}}</div>
-                <div class="regularButtons">{{keyboard[42].meaning}}</div>
-                <div class="regularButtons">{{keyboard[43].meaning}}</div>
-                <div class="regularButtons">{{keyboard[44].meaning}}</div>
-                <div class="regularButtons">{{keyboard[45].meaning}}</div>
-                <div class="regularButtons">{{keyboard[46].meaning}}</div>
-                <div class="regularButtons">{{keyboard[47].meaning}}</div>
-                <div class="regularButtons">{{keyboard[48].meaning}}</div>
-                <div class="regularButtons">{{keyboard[49].meaning}}</div>
-                <div class="regularButtons">{{keyboard[50].meaning}}</div>
-                <div class="regularButtons grayButtons">{{keyboard[51].meaning}}</div>
-            </div>
+  <div class="fourth">
+    <h1>Eyes on the Screen</h1>
+    <div class="content">
+      <div class="line">
+        <div
+          class="regular-buttons"
+          v-for="item in keyboard_first_line"
+          :key="item.id"
+          :style="{
+            '--key-color': item.color,
+            '--key-background-color': item.background,
+          }"
+        >
+          {{ item.meaning }}
         </div>
+      </div>
+      <div class="line">
+        <div
+          class="regular-buttons"
+          v-for="item in keyboard_second_line"
+          :key="item.id"
+          :style="{
+            '--key-color': item.color,
+            '--key-background-color': item.background,
+          }"
+        >
+          {{ item.meaning }}
+        </div>
+      </div>
+      <div class="line">
+        <div
+          class="regular-buttons"
+          v-for="item in keyboard_third_line"
+          :key="item.id"
+          :style="{
+            '--key-color': item.color,
+            '--key-background-color': item.background,
+          }"
+        >
+          {{ item.meaning }}
+        </div>
+      </div>
+      <div class="line">
+        <div
+          class="regular-buttons"
+          v-for="item in keyboard_fourth_line"
+          :key="item.id"
+          :style="{
+            '--key-color': item.color,
+            '--key-background-color': item.background,
+          }"
+        >
+          {{ item.meaning }}
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-export default{
-    colorMode: "fourth",
+export default {
+  colorMode: "fourth",
+  beforeMount() {
+    this.randomKey();
+  },
   data: function () {
     return {
-      keyboard: [
+      keyboard_first_line: [
         {
           id: 1,
-          meaning:"`",
+          meaning: "`",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 2,
-          meaning:1,
+          meaning: 1,
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 3,
-          meaning:2,
+          meaning: 2,
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 4,
-          meaning:3,
+          meaning: 3,
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 5,
-          meaning:4,
+          meaning: 4,
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 6,
-          meaning:5,
+          meaning: 5,
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 7,
-          meaning:6,
+          meaning: 6,
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 8,
-          meaning:7,
+          meaning: 7,
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 9,
-          meaning:8,
+          meaning: 8,
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 10,
-          meaning:9,
+          meaning: 9,
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 11,
-          meaning:0,
+          meaning: 0,
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 12,
-          meaning:"-",
+          meaning: "-",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 13,
-          meaning:"=",
+          meaning: "=",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 14,
-          meaning:"DEL",
+          meaning: "DEL",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
+      ],
+      keyboard_second_line: [
         {
           id: 15,
-          meaning:"Tab",
+          meaning: "Tab",
+          color: "#868888",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 16,
-          meaning:"Q",
+          meaning: "Q",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 17,
-          meaning:"E",
+          meaning: "E",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 18,
-          meaning:"R",
+          meaning: "R",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 19,
-          meaning:"T",
+          meaning: "T",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 20,
-          meaning:"Y",
+          meaning: "Y",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 21,
-          meaning:"U",
+          meaning: "U",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 22,
-          meaning:"I",
+          meaning: "I",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 23,
-          meaning:"O",
+          meaning: "O",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 24,
-          meaning:"P",
+          meaning: "P",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 25,
-          meaning:"[",
+          meaning: "[",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 26,
-          meaning:"]",
+          meaning: "]",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 27,
-          meaning:"\\",
+          meaning: "\\",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
+      ],
+      keyboard_third_line: [
         {
           id: 28,
-          meaning:"CAPS",
+          meaning: "CAPS",
+          color: "#868888",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 29,
-          meaning:"A",
+          meaning: "A",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 30,
-          meaning:"S",
+          meaning: "S",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 31,
-          meaning:"D",
+          meaning: "D",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 32,
-          meaning:"F",
+          meaning: "F",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 33,
-          meaning:"G",
+          meaning: "G",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 34,
-          meaning:"H",
+          meaning: "H",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 35,
-          meaning:"j",
+          meaning: "j",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 36,
-          meaning:"K",
+          meaning: "K",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 37,
-          meaning:"L",
+          meaning: "L",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 38,
-          meaning:";",
+          meaning: ";",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 39,
-          meaning:"'",
+          meaning: "'",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 40,
-          meaning:"ENTER",
+          meaning: "ENTER",
+          color: "#868888",
+          background: "#FFFFFF",
           chosen: false,
         },
+      ],
+      keyboard_fourth_line: [
         {
           id: 41,
-          meaning:"SHIFT",
+          meaning: "SHIFT",
+          color: "#868888",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 42,
-          meaning:"Z",
+          meaning: "Z",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 43,
-          meaning:"X",
+          meaning: "X",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 44,
-          meaning:"C",
+          meaning: "C",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 45,
-          meaning:"V",
+          meaning: "V",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 45,
-          meaning:"B",
+          meaning: "B",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 46,
-          meaning:"N",
+          meaning: "N",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 47,
-          meaning:"M",
+          meaning: "M",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 48,
-          meaning:",",
+          meaning: ",",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 49,
-          meaning:".",
+          meaning: ".",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 50,
-          meaning:"/",
+          meaning: "/",
+          color: "#60C1B6",
+          background: "#FFFFFF",
           chosen: false,
         },
         {
           id: 51,
-          meaning:"SHIFT",
+          meaning: "SHIFT",
+          color: "#868888",
+          background: "#FFFFFF",
           chosen: false,
         },
       ],
     };
   },
-  methods:{
-   
+  methods: {
+    randomKey: function () {
+      let min = 1;
+      let max = 14;
+      let randomnumber = Math.floor(Math.random() * (max - min)) + min;
+      console.log(randomnumber);
+      const elem = this.keyboard_first_line.find(function (element) {
+        return element.id === randomnumber;
+      });
+      elem.classList.add("test");
+    },
   },
-    methods: {
-      beforeMount(){
-        this.randomKey()
-        console.log(aaaaaaa);
-      },
-      randomKey: function(){
-        let key = keyboard[Math.floor(Math.random()*keyboard.length)];
-        console.log(key);
-        return key;
-      },
-   },
-}
+};
 </script>
 <style scoped lang="scss">
 @import "./../assets/styles/fourth.css";
