a158ab324f92fba4ace7a3eeea6732e56e00f9dc
bojar <michael.sagel89@gmail.com>
2022-02-23 15:11:32 +0100

    keyboard

--- pages/fourth.vue
+++ pages/fourth.vue
@@ -2,56 +2,24 @@
   <div class="fourth">
     <h1>Eyes on the Screen</h1>
     <div class="content">
-      <div class="line">
+      <div 
+        class="line"
+        v-for="item in keyboard"
+        :key="item.id"
+      >
         <div
           class="regular-buttons"
-          v-for="item in keyboard_first_line"
-          :key="item.id"
+          v-for="element in item.line"
+          :key="element.id"
+          :class="{
+            play:selectedId === element.id
+          }"
           :style="{
-            '--key-color': item.color,
-            '--key-background-color': item.background,
+            '--key-color': element.color,
+            '--key-background-color': element.background,
           }"
         >
-          {{ item.meaning }}
-        </div>
-      </div>
-      <div class="line">
-        <div
-          class="regular-buttons"
-          v-for="item in keyboard_second_line"
-          :key="item.id"
-          :style="{
-            '--key-color': item.color,
-            '--key-background-color': item.background,
-          }"
-        >
-          {{ item.meaning }}
-        </div>
-      </div>
-      <div class="line">
-        <div
-          class="regular-buttons"
-          v-for="item in keyboard_third_line"
-          :key="item.id"
-          :style="{
-            '--key-color': item.color,
-            '--key-background-color': item.background,
-          }"
-        >
-          {{ item.meaning }}
-        </div>
-      </div>
-      <div class="line">
-        <div
-          class="regular-buttons"
-          v-for="item in keyboard_fourth_line"
-          :key="item.id"
-          :style="{
-            '--key-color': item.color,
-            '--key-background-color': item.background,
-          }"
-        >
-          {{ item.meaning }}
+          {{ element.value }}
         </div>
       </div>
     </div>
@@ -65,390 +33,354 @@ export default {
   },
   data: function () {
     return {
-      keyboard_first_line: [
+      selectedId:0,
+      keyboard: [
         {
-          id: 1,
-          meaning: "`",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
+          id:0,
+          line: [
+            {
+              id: 1,
+              value: "`",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 2,
+              value: 1,
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 3,
+              value: 2,
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 4,
+              value: 3,
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 5,
+              value: 4,
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 6,
+              value: 5,
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 7,
+              value: 6,
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 8,
+              value: 7,
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 9,
+              value: 8,
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 10,
+              value: 9,
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 11,
+              value: 0,
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 12,
+              value: "-",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 13,
+              value: "=",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 14,
+              value: "DEL",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+          ],
         },
         {
-          id: 2,
-          meaning: 1,
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
+          id:1,
+          line: [
+            {
+              id: 15,
+              value: "Tab",
+              color: "#868888",
+              background: "#FFFFFF",
+            },
+            {
+              id: 16,
+              value: "Q",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 17,
+              value: "E",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 18,
+              value: "R",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 19,
+              value: "T",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 20,
+              value: "Y",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 21,
+              value: "U",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 22,
+              value: "I",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 23,
+              value: "O",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 24,
+              value: "P",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 25,
+              value: "[",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 26,
+              value: "]",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 27,
+              value: "\\",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+          ],
         },
         {
-          id: 3,
-          meaning: 2,
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
+          id:2,
+          line: [
+            {
+              id: 28,
+              value: "CAPS",
+              color: "#868888",
+              background: "#FFFFFF",
+            },
+            {
+              id: 29,
+              value: "A",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 30,
+              value: "S",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 31,
+              value: "D",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 32,
+              value: "F",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 33,
+              value: "G",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 34,
+              value: "H",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 35,
+              value: "j",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 36,
+              value: "K",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 37,
+              value: "L",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 38,
+              value: ";",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 39,
+              value: "'",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 40,
+              value: "ENTER",
+              color: "#868888",
+              background: "#FFFFFF",
+            },
+          ],
         },
         {
-          id: 4,
-          meaning: 3,
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
+          id:3,
+          line: [
+            {
+              id: 41,
+              value: "SHIFT",
+              color: "#868888",
+              background: "#FFFFFF",
+            },
+            {
+              id: 42,
+              value: "Z",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 43,
+              value: "X",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 44,
+              value: "C",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 45,
+              value: "V",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 45,
+              value: "B",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 46,
+              value: "N",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 47,
+              value: "M",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 48,
+              value: ",",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 49,
+              value: ".",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 50,
+              value: "/",
+              color: "#60C1B6",
+              background: "#FFFFFF",
+            },
+            {
+              id: 51,
+              value: "SHIFT",
+              color: "#868888",
+              background: "#FFFFFF",
+            },
+          ],
         },
-        {
-          id: 5,
-          meaning: 4,
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 6,
-          meaning: 5,
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 7,
-          meaning: 6,
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 8,
-          meaning: 7,
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 9,
-          meaning: 8,
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 10,
-          meaning: 9,
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 11,
-          meaning: 0,
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 12,
-          meaning: "-",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 13,
-          meaning: "=",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 14,
-          meaning: "DEL",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-      ],
-      keyboard_second_line: [
-        {
-          id: 15,
-          meaning: "Tab",
-          color: "#868888",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 16,
-          meaning: "Q",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 17,
-          meaning: "E",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 18,
-          meaning: "R",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 19,
-          meaning: "T",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 20,
-          meaning: "Y",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 21,
-          meaning: "U",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 22,
-          meaning: "I",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 23,
-          meaning: "O",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 24,
-          meaning: "P",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 25,
-          meaning: "[",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 26,
-          meaning: "]",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 27,
-          meaning: "\\",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-      ],
-      keyboard_third_line: [
-        {
-          id: 28,
-          meaning: "CAPS",
-          color: "#868888",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 29,
-          meaning: "A",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 30,
-          meaning: "S",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 31,
-          meaning: "D",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 32,
-          meaning: "F",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 33,
-          meaning: "G",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 34,
-          meaning: "H",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 35,
-          meaning: "j",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 36,
-          meaning: "K",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 37,
-          meaning: "L",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 38,
-          meaning: ";",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 39,
-          meaning: "'",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 40,
-          meaning: "ENTER",
-          color: "#868888",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-      ],
-      keyboard_fourth_line: [
-        {
-          id: 41,
-          meaning: "SHIFT",
-          color: "#868888",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 42,
-          meaning: "Z",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 43,
-          meaning: "X",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 44,
-          meaning: "C",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 45,
-          meaning: "V",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 45,
-          meaning: "B",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 46,
-          meaning: "N",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 47,
-          meaning: "M",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 48,
-          meaning: ",",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 49,
-          meaning: ".",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 50,
-          meaning: "/",
-          color: "#60C1B6",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-        {
-          id: 51,
-          meaning: "SHIFT",
-          color: "#868888",
-          background: "#FFFFFF",
-          chosen: false,
-        },
-      ],
+      ],      
     };
   },
   methods: {
     randomKey: function () {
       let min = 1;
-      let max = 14;
+      let max = 51;
       let randomnumber = Math.floor(Math.random() * (max - min)) + min;
       console.log(randomnumber);
-      const elem = this.keyboard_first_line.find(function (element) {
-        return element.id === randomnumber;
-      });
-      elem.classList.add("test");
+      this.selectedId = randomnumber;
+      let pressedButton = document.addEventListener('keydown');
+      if (pressedButton === element.value){
+
+      }
     },
   },
 };
