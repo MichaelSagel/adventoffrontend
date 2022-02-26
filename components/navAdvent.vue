7c8e1cd674d1b502d49a45824ad35cba79f06f88
bojar <michael.sagel89@gmail.com>
2022-02-26 12:47:10 +0100

    podcast

--- components/navAdvent.vue
+++ components/navAdvent.vue
@@ -1,46 +1,21 @@
 <template>
   <nav class="navigation">
-    <div 
-      class="navigationPanel"
-    >
-      <NuxtLink
-        to="/" 
-        class="navigationPanelElement"
-      >
-        Home
-      </NuxtLink>
+    <div class="navigationPanel">
+      <NuxtLink to="/" class="navigationPanelElement"> Home </NuxtLink>
 
-      <NuxtLink
-        to="/first"
-        class="navigationPanelElement"
-      >
-        First
-      </NuxtLink>
-      
-      <NuxtLink
-        to="/second"
-        class="navigationPanelElement"
-      >
-        Second
-      </NuxtLink>
+      <NuxtLink to="/first" class="navigationPanelElement"> First </NuxtLink>
 
-      <NuxtLink
-          to="/third" 
-          class="navigationPanelElement"
-        >
-          Third
-        </NuxtLink>
+      <NuxtLink to="/second" class="navigationPanelElement"> Second </NuxtLink>
 
-        <NuxtLink
-          to="/fourth" 
-          class="navigationPanelElement"
-        >
-          Fourth
-        </NuxtLink>
+      <NuxtLink to="/third" class="navigationPanelElement"> Third </NuxtLink>
+
+      <NuxtLink to="/fourth" class="navigationPanelElement"> Fourth </NuxtLink>
+
+      <NuxtLink to="/fifth" class="navigationPanelElement"> Fifth </NuxtLink>
     </div>
   </nav>
 </template>
 
 <style>
-@import './../assets/styles/navigation.css';
+@import "./../assets/styles/navigation.css";
 </style>
